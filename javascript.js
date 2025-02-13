ad4ae7d24cced02cd77bba63755b29b595cbb863
yadhuks42 <yadhuks42@gmail.com>
2025-02-13 15:25:36 +0000

    On new branch ternary-op Optimize code with switchcases for victory, add separate functions for logic, scoring, and game rounds.

--- javascript.js
+++ javascript.js
@@ -44,58 +44,125 @@ function getPlayerChoice(){
     
 }
 
+function logic(pc,cc){
+    if(pc === cc){
+        let result = "Draw.";
+        return result;
+    }
+    else{
+        switch (pc) {
+            case "rock":{
+                let result = (cc==="scissors")?"Rock beats scissors.":"Paper beats Rock.";
+                return result;
+                
+            }
+            
+            case "paper":{
+                let result = (cc==="rock")?"Paper beats Rock.":"Scissors beats Paper.";
+                return result;
+                
+            }
+                
+            case "scissors":{
+                let result = (cc==="paper")?"Scissors beat Paper.":"Rock beats Scissors";
+                return result;
+                
+            }
+            default:
+                break;
+        }
+    }    
+}
+
+function scoring(result){
+
+    if(result === "Rock beats scissors." || result === "Paper beats Rock." || result ==="Scissors beat Paper." ){
+        playerScore++;
+        return("You Win.");
+    }
+    else if(result === "Draw."){
+        return "No points.";
+    }
+    else{
+        compScore++;
+        return("Computer Wins.");
+    }
+}
+
 function playGame(){
 
     compScore = 0;
     playerScore = 0;
 
     for(let i=1; i<6; i++){
+            console.log("Round " + i);
             console.log("waiting for player");
             let pc = getPlayerChoice();
             let cc = getComputerChoice();
 
-            if(pc === cc){
-                console.log("Draw.");
-            }
-            else if(pc ==="rock" && cc==="paper"){
-                compScore++;
-                console.log("Paper beats Rock. Computer Wins.");
-            }
-            else if(pc ==="rock" && cc==="scissors"){
-                playerScore++;
-                console.log("Rock beats scissors. You Win.");
-            }
-            else if(pc ==="paper" && cc==="rock"){
-                playerScore++;
-                console.log("Paper beats Rock. You Win.");
-            }
-            else if(pc ==="paper" && cc==="scissors"){
-                compScore++;
-                console.log("Scissors beats Paper. Computer Wins.");
-            }
-            else if(pc ==="scissors" && cc==="rock"){
-                compScore++;
-                console.log("Rock beats Scissors. Computer Wins.");
-            }
-            else if(pc ==="scissors" && cc==="paper"){
-                playerScore++;
-                console.log("Scissors beats Paper. You Win.");
-            }
-            console.log('End of Round ' + i + '\n'+'Score: Player - '+ playerScore+' Computer - '+ compScore);
-    }
-
-            if(compScore===playerScore){
-                console.log("Draw.");
-            }
-            else if(compScore>playerScore){
-                console.log("Computer Wins.");
-            }
-            else if(playerScore>compScore){
-                console.log("Congratulations! You won.");
-            }
+            let output = logic(pc,cc);
+            console.log(output);
+            let result = scoring(output);
+            console.log(result);
+            console.log("Player Score - "+ playerScore + " Computer Score - " + compScore);
+        }
     
- 
-}
+    if(compScore===playerScore){
+        console.log("Game ends in a Draw.");
+    }
+    else if(playerScore>compScore){
+        console.log("Congratulations! You won the game.");
+    }
+    else if(compScore>playerScore){
+        console.log("Computer wins the game.");
+    }
+        
+}       
+            
+
+
+    //         if(pc === cc){
+    //             console.log("Draw.");
+    //         }
+    //         else if(pc ==="rock" && cc==="paper"){
+    //             compScore++;
+    //             console.log("Paper beats Rock. Computer Wins.");
+    //         }
+    //         else if(pc ==="rock" && cc==="scissors"){
+    //             playerScore++;
+    //             console.log("Rock beats scissors. You Win.");
+    //         }
+    //         else if(pc ==="paper" && cc==="rock"){
+    //             playerScore++;
+    //             console.log("Paper beats Rock. You Win.");
+    //         }
+    //         else if(pc ==="paper" && cc==="scissors"){
+    //             compScore++;
+    //             console.log("Scissors beats Paper. Computer Wins.");
+    //         }
+    //         else if(pc ==="scissors" && cc==="rock"){
+    //             compScore++;
+    //             console.log("Rock beats Scissors. Computer Wins.");
+    //         }
+    //         else if(pc ==="scissors" && cc==="paper"){
+    //             playerScore++;
+    //             console.log("Scissors beats Paper. You Win.");
+    //         }
+    //         console.log('End of Round ' + i + '\n'+'Score: Player - '+ playerScore+' Computer - '+ compScore);
+    // }
+
+    //         if(compScore===playerScore){
+    //             console.log("Draw.");
+    //         }
+    //         else if(compScore>playerScore){
+    //             console.log("Computer Wins.");
+    //         }
+    //         else if(playerScore>compScore){
+    //             console.log("Congratulations! You won.");
+    //       }
+
+             
+//}
 
 function start(){
     let entry = prompt("ROCK PAPER SCISSORS. TYPE START TO BEGIN.");
